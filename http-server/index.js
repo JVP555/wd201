7bc5840bda86a5ec2555115593451a24564f70df
JVP555 <jilvarghese.2005@gmail.com>
2025-03-28 21:40:47 +0530

    Updated index.js with dynamic port functionality

--- http-server/index.js
+++ http-server/index.js
@@ -1,42 +1,63 @@
 const http = require("http");
 const fs = require("fs");
+const minimist = require("minimist");
 
+// Parse command-line arguments
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000; // Default port if not provided
+
+// Store content in variables
 let homeContent = "";
 let projectContent = "";
 let registrationContent = "";
 
-fs.readFile("home.html", (err, home) => {
-  if (err) throw err;
-  homeContent = home;
+// Read HTML files asynchronously
+fs.readFile("home.html", (err, data) => {
+  if (err) {
+    console.error("Error reading home.html:", err);
+    return;
+  }
+  homeContent = data;
 });
 
-fs.readFile("project.html", (err, project) => {
-  if (err) throw err;
-  projectContent = project;
+fs.readFile("project.html", (err, data) => {
+  if (err) {
+    console.error("Error reading project.html:", err);
+    return;
+  }
+  projectContent = data;
 });
 
-fs.readFile("registration.html", (err, registration) => {
-  if (err) throw err;
-  registrationContent = registration;
+fs.readFile("registration.html", (err, data) => {
+  if (err) {
+    console.error("Error reading registration.html:", err);
+    return;
+  }
+  registrationContent = data;
 });
 
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
+// Create the server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
 
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        break;
-      case "/registration":
-        response.write(registrationContent);
-        break;
-      default:
-        response.write(homeContent);
-        break;
-    }
+  switch (req.url) {
+    case "/project":
+      res.end(projectContent);
+      break;
+    case "/registration":
+      res.end(registrationContent);
+      break;
+    case "/":
+      res.end(homeContent);
+      break;
+    default:
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>404 - Not Found</h1>");
+      break;
+  }
+});
 
-    response.end();
-  })
-  .listen(5000);
+// Start server with dynamic port
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
